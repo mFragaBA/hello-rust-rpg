1c6e2744986a0fe99f4ff4ee4c4460b50507112d
Martin Fraga <martin.fraga@lambdaclass.com>
2022-05-31 22:56:07 -0300

    initial commit and creating components

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+
+struct State {
+    ecs: World
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+
+    // Initialize Game State
+    let gs = State { 
+        World::new()
+    };
+
+    // Register Components
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    rltk::main_loop(context, gs)
+}
