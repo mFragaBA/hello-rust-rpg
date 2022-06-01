f6ce616da2629386a2f83843db8347b512d4acf6
Martin Fraga <martin.fraga@lambdaclass.com>
2022-06-01 00:03:12 -0300

    implement player movement

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,9 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
 struct State {
     ecs: World
 }
@@ -49,6 +52,7 @@ impl GameState for State {
         ctx.cls();
         ctx.print(1, 1, "Hello Rust World");
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -56,6 +60,30 @@ impl GameState for State {
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing Happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
         }
     }
 }
@@ -75,6 +103,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     // Create Entities
     gs.ecs
@@ -99,6 +128,17 @@ fn main() -> rltk::BError {
             .with(LeftMover{})
             .build();
     }
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player {})
+        .build();
 
     rltk::main_loop(context, gs)
 }
